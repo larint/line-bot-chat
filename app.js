24c784cd50728bc6d62dda78dfa79c846893ae23
Dung <qdb611@gmail.com>
2020-08-13 08:19:09 +0700

    make it better"

---
+++ app.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const morgan = require("morgan");
+const nodeSchedule = require("node-schedule");
+const fs = require("fs");
+const path = require("path");
+const LineSchedule_1 = require("./services/LineSchedule");
+require("./helpers/db");
+const index_1 = require("./routes/index");
+const users_1 = require("./routes/users");
+const webhook_1 = require("./routes/webhook");
+const crawler_1 = require("./routes/crawler");
+const test_1 = require("./routes/test");
+require('dotenv').config();
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+var appLogStream = fs.createWriteStream(path.join(__dirname, 'app.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: appLogStream, skip: (req, res) => { return res.statusCode < 400; } }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: "bjhbahsbdjabwdhjbwjdh", resave: true, saveUninitialized: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+    res.locals.site_url = process.env.URI_PATH;
+    next();
+});
+app.use(/\/(app.js|package.json)/, (req, res, next) => {
+    res.sendStatus(404);
+});
+app.use('/', index_1.router);
+app.use('/users', users_1.router);
+app.use('/webhook', webhook_1.router);
+app.use('/crawler', crawler_1.router);
+app.use('/test', test_1.router);
+app.use((err, req, res, next) => {
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.render('error');
+});
+nodeSchedule.scheduleJob('* * * * *', function () {
+});
+LineSchedule_1.LineSchedule.run();
+app.listen(3000, () => console.log('listening @ 3000', new Date()));
---
+++ services/LineSchedule.js
@@ -0,0 +1,227 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LineSchedule = void 0;
+const bot_sdk_1 = require("@line/bot-sdk");
+const helper_1 = require("../helpers/helper");
+const faker_1 = require("../faker/faker");
+const db_1 = require("../helpers/db");
+const friend_graphics__genders_1 = require("../migrations//tables/friend_graphics__genders");
+const friend_graphics__ages_1 = require("../migrations//tables/friend_graphics__ages");
+const friend_graphics__apptypes_1 = require("../migrations//tables/friend_graphics__apptypes");
+const friend_graphics__subscriptions_1 = require("../migrations//tables/friend_graphics__subscriptions");
+const friend_graphics__areas_1 = require("../migrations//tables/friend_graphics__areas");
+require('dotenv').config();
+const config = {
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_CHANNEL_SECRET
+};
+const client = new bot_sdk_1.Client(config);
+class LineSchedule {
+}
+exports.LineSchedule = LineSchedule;
+LineSchedule.run = async () => {
+    console.log('run getFriendDemographics ' + new Date());
+    let friend = await faker_1.Faker.getFriendGraphics('jp');
+    await LineSchedule.saveGraphicsGenders(friend);
+    await LineSchedule.saveGraphicsAges(friend);
+    await LineSchedule.saveGraphicsAppTypes(friend);
+    await LineSchedule.saveGraphicsSubscription(friend);
+    await LineSchedule.saveGraphicsAreas(friend);
+    await LineSchedule.saveMessageStatistic();
+};
+LineSchedule.saveGraphicsGenders = async (friend) => {
+    var _a, _b;
+    let currentDate = helper_1.formatDate('YYYYMMDD');
+    let exist = await db_1.DB.selectByParams({
+        select: 'id',
+        table: 'friend_graphics__genders',
+        set: '??=?',
+        where: ['date_update', currentDate]
+    });
+    if (exist.length > 0) {
+        return;
+    }
+    let gendersWhere = ['date_update', currentDate];
+    (_a = friend.genders) === null || _a === void 0 ? void 0 : _a.forEach((item) => {
+        gendersWhere.push(item.gender);
+        gendersWhere.push(item.percentage);
+    });
+    if (((_b = friend.genders) === null || _b === void 0 ? void 0 : _b.length) == 0) {
+        friend_graphics__genders_1.TableFriendGraphicsGenders.column.slice(2).forEach((column, idx) => {
+            gendersWhere.push(column);
+            gendersWhere.push('0');
+        });
+    }
+    await db_1.DB.insertItem({
+        table: 'friend_graphics__genders',
+        set: '?? = ?,?? = ?,?? = ?,?? = ?',
+        where: gendersWhere
+    });
+};
+LineSchedule.saveGraphicsAges = async (friend) => {
+    var _a, _b;
+    let currentDate = helper_1.formatDate('YYYYMMDD');
+    let exist = await db_1.DB.selectByParams({
+        select: 'id',
+        table: 'friend_graphics__ages',
+        set: '??=?',
+        where: ['date_update', currentDate]
+    });
+    if (exist.length > 0) {
+        return;
+    }
+    let agesWhere = ['date_update', currentDate];
+    (_a = friend.ages) === null || _a === void 0 ? void 0 : _a.forEach((item) => {
+        agesWhere.push(item.age);
+        agesWhere.push(item.percentage);
+    });
+    if (((_b = friend.ages) === null || _b === void 0 ? void 0 : _b.length) == 0) {
+        friend_graphics__ages_1.TableFriendGraphicsAges.column.slice(2).forEach((column, idx) => {
+            agesWhere.push(column);
+            agesWhere.push('0');
+        });
+    }
+    await db_1.DB.insertItem({
+        table: 'friend_graphics__ages',
+        set: '?? = ?,?? = ?,?? = ?,?? = ?,?? = ?,?? = ?,?? = ?,?? = ?,?? = ?,?? = ?',
+        where: agesWhere
+    });
+};
+LineSchedule.saveGraphicsAppTypes = async (friend) => {
+    var _a, _b;
+    let currentDate = helper_1.formatDate('YYYYMMDD');
+    let exist = await db_1.DB.selectByParams({
+        select: 'id',
+        table: 'friend_graphics__apptypes',
+        set: '??=?',
+        where: ['date_update', currentDate]
+    });
+    if (exist.length > 0) {
+        return;
+    }
+    let appTypesWhere = ['date_update', currentDate];
+    (_a = friend.appTypes) === null || _a === void 0 ? void 0 : _a.forEach((item) => {
+        appTypesWhere.push(item.appType);
+        appTypesWhere.push(item.percentage);
+    });
+    if (((_b = friend.appTypes) === null || _b === void 0 ? void 0 : _b.length) == 0) {
+        friend_graphics__apptypes_1.TableFriendGraphicsApptypes.column.slice(2).forEach((column, idx) => {
+            appTypesWhere.push(column);
+            appTypesWhere.push('0');
+        });
+    }
+    await db_1.DB.insertItem({
+        table: 'friend_graphics__apptypes',
+        set: '?? = ?,?? = ?,?? = ?,?? = ?',
+        where: appTypesWhere
+    });
+};
+LineSchedule.saveGraphicsSubscription = async (friend) => {
+    var _a, _b;
+    let currentDate = helper_1.formatDate('YYYYMMDD');
+    let exist = await db_1.DB.selectByParams({
+        select: 'id',
+        table: 'friend_graphics__subscriptions',
+        set: '??=?',
+        where: ['date_update', currentDate]
+    });
+    if (exist.length > 0) {
+        return;
+    }
+    let subscriptionWhere = ['date_update', currentDate];
+    (_a = friend.subscriptionPeriods) === null || _a === void 0 ? void 0 : _a.forEach((item) => {
+        subscriptionWhere.push(item.subscriptionPeriod);
+        subscriptionWhere.push(item.percentage);
+    });
+    if (((_b = friend.subscriptionPeriods) === null || _b === void 0 ? void 0 : _b.length) == 0) {
+        friend_graphics__subscriptions_1.TableFriendGraphicsSubscriptions.column.slice(2).forEach((column, idx) => {
+            subscriptionWhere.push(column);
+            subscriptionWhere.push('0');
+        });
+    }
+    await db_1.DB.insertItem({
+        table: 'friend_graphics__subscriptions',
+        set: '?? = ?,?? = ?,?? = ?,?? = ?,?? = ?,?? = ?,?? = ?,?? = ?',
+        where: subscriptionWhere
+    });
+};
+LineSchedule.saveGraphicsAreas = async (friend) => {
+    var _a, _b;
+    let currentDate = helper_1.formatDate('YYYYMMDD');
+    let exist = await db_1.DB.selectByParams({
+        select: 'id',
+        table: 'friend_graphics__areas',
+        set: '??=?',
+        where: ['date_update', currentDate]
+    });
+    if (exist.length > 0) {
+        return;
+    }
+    let areasWhere = ['date_update', currentDate];
+    let areaTrans = friend_graphics__areas_1.TableFriendGraphicsAreas.areaTrans;
+    let set = '?? = ?,';
+    (_a = friend.areas) === null || _a === void 0 ? void 0 : _a.forEach((item) => {
+        areasWhere.push(areaTrans[item.area]);
+        areasWhere.push(item.percentage);
+        set += '?? = ?,';
+    });
+    let citys = Object.values(areaTrans);
+    if (((_b = friend.areas) === null || _b === void 0 ? void 0 : _b.length) == 0) {
+        citys.map((city) => {
+            areasWhere.push(city);
+            areasWhere.push('0');
+        });
+    }
+    await db_1.DB.insertItem({
+        table: 'friend_graphics__areas',
+        set: set.slice(0, -1),
+        where: areasWhere
+    });
+};
+LineSchedule.saveMessageStatistic = async () => {
+    let currentDate = helper_1.formatDate('YYYYMMDD');
+    let exist = await db_1.DB.selectByParams({
+        select: 'id',
+        table: 'messages_statistic',
+        set: '??=?',
+        where: ['date_update', currentDate]
+    });
+    if (exist.length > 0) {
+        return;
+    }
+    let result = {};
+    result.reply = await client.getNumberOfSentReplyMessages(currentDate);
+    result.sentPush = await client.getNumberOfSentPushMessages(currentDate);
+    result.sentMulticast = await client.getNumberOfSentMulticastMessages(currentDate);
+    result.sentBroadcast = await client.getNumberOfSentBroadcastMessages(currentDate);
+    result.messageDeliveries = await client.getNumberOfMessageDeliveries(currentDate);
+    await db_1.DB.insertItem({
+        table: 'messages_statistic',
+        set: '??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?,??=?',
+        where: [
+            'date_update', currentDate,
+            'reply_status', result.reply.status,
+            'reply_number',
+            result.reply.success,
+            'push_status', result.sentPush.status,
+            'push_number',
+            result.sentPush.success,
+            'multicast_status', result.sentMulticast.status,
+            'multicast_number',
+            result.sentMulticast.success,
+            'broadcast_status', result.sentBroadcast.status,
+            'broadcast_number',
+            result.sentBroadcast.success,
+            'deliveries_status', result.messageDeliveries.status,
+            'deliveries_broadcast', result.messageDeliveries.broadcast,
+            'deliveries_targeting', result.messageDeliveries.targeting,
+            'deliveries_auto_response', result.messageDeliveries.autoResponse,
+            'deliveries_welcome_response', result.messageDeliveries.welcomeResponse,
+            'deliveries_chat', result.messageDeliveries.chat,
+            'deliveries_api_broadcast', result.messageDeliveries.apiBroadcast,
+            'deliveries_api_push', result.messageDeliveries.apiPush,
+            'deliveries_api_multicast', result.messageDeliveries.welcomeResponse,
+            'deliveries_api_reply', result.messageDeliveries.apiReply,
+        ]
+    });
+};
